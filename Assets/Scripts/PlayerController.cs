105cad49c9fabbfab2f4361cd5490ce1b1452ba1
Lucrezia Tasso <tassolucrezia@gmail.com>
2025-10-24 12:36:48 +0200

    Spawn Animals

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float verticalInput;
     public float speed = 10.0f;
+    public float xRange = 10;
+    public GameObject projectilePrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,15 +24,20 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
 
-        if (transform.position.x < -10)
+        if (transform.position.x < -xRange)
         {
-           transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+           transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
 
-        if (transform.position.x > 10)
+        if (transform.position.x > xRange)
         {
-                transform.position = new Vector3(10, transform.position.y, transform.position.z);
+                transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
             }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
 
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+using Unity.Android.Gradle;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], spawnPos,
+                animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
